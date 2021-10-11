e842938bea37e03bf5e4063fd52df503dd53c498
RuslanMirov <ruslan777mirov@gmail.com>
2021-10-11 11:22:09 +0300

    Update token with mint/burn

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,9 +2,15 @@
 pragma solidity ^0.6.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20 {
-    constructor(uint256 _initSupply) public ERC20("COT", "COT") {
+contract Token is ERC20, ERC20Burnable, Ownable {
+    constructor(uint256 _initSupply) public ERC20("CoTrader", "COT") {
         _mint(msg.sender, _initSupply);
+    }
+
+    function mint( address _to, uint256 _amount) external onlyOwner {
+      _mint(_to, _amount);
     }
 }
